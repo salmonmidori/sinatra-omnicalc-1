b0a97112376a47f97ae72f169ac96928c37d55e6
Amy May Nguyen <anguye10@uchicago.edu>
2025-01-30 17:53:14 +0000

    Completed square pages

--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square_new)
+end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/square/results") do
+  @input = params["number"]
+  @result = @input.to_f ** 2
+  erb(:square) 
 end
